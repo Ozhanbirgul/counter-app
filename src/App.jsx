a81e51739e29a2f6581eab4f9ab23d47fd47c14b
Birgül Özhan <birgulozhan9@gmail.com>
2025-12-10 22:59:19 +0300

    useEffect kullanıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useEffect } from "react";
 import { useState } from "react"
 
 function App() {
 const [count, setCount] = useState(0);
 
+useEffect(() => {
+  console.log("Count değişti:", count);
+  if(count>5) {
+    alert("Count 5'i geçti")
+  }
+}, [count])
+
   return (
     <div>
      <h1>Count: {count}</h1>
-     <button onClick={() => setCount(count + 1)}>Increase</button>
+     <button onClick={() => setCount(prev => prev +1)}>Increase</button>
     </div>
   )
 }
